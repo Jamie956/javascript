55dbfcb49b3018d253cf63532d6ee0a052d2a5ab
Unknown <752492509@qq.com>
2018-06-30 13:40:19 +0800

    update

--- algo/13-sort.js
+++ algo/13-sort.js
@@ -81,6 +81,25 @@ function ArrayList() {
       right = array.slice(mid, length); //{5}
     return merge(mergeSortRec(left), mergeSortRec(right)); //{6}
   };
+  var merge = function (left, right) {
+    var result = [], // {7}
+      il = 0,
+      ir = 0;
+    while (il < left.length && ir < right.length) { // {8}
+      if (left[il] < right[ir]) {
+        result.push(left[il++]); // {9}
+      } else {
+        result.push(right[ir++]); // {10}
+      }
+    }
+    while (il < left.length) {
+      result.push(left[il++]);
+    }
+    while (ir < right.length) {
+      result.push(right[ir++]);
+    }
+    return result; // {13}
+  };
 }
 
 //生成测试数组
