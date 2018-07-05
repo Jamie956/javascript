a35325503d1c521d6e64ca701e81675353b6472c
Unknown <752492509@qq.com>
2018-07-06 00:25:42 +0800

    update

---
+++ 01-callback.js
@@ -0,0 +1,9 @@
+function foo(cb) {
+    cb();//execute cb
+}
+foo(function () { console.log('foo callback') });
+
+function bar(cb) {
+    cb.call(this);//execute cb by call
+}
+bar(function () { console.log('bar callback') });	
