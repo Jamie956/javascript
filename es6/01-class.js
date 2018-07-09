56931d41d2c1c82f110e28111fb51b2f919f9372
Unknown <752492509@qq.com>
2018-07-09 08:06:31 +0800

    update

--- es6/01-class.js
+++ es6/01-class.js
@@ -1,3 +1,4 @@
+//函数定义变量，方法
 class Point {
   // constructor 构造方法
   // 默认返回实例对象（即this）
@@ -13,26 +14,25 @@ class Point {
 }
 
 var p = new Point(2, 3); //必须new
-var rs = p.toString();
+p.toString();
 
-//
-
-let person = new class {
+//定义匿名class，绑定变量
+let person = new class {//创建匿名class，赋值给变量
   constructor(name) {
     this.name = name;
   }
   sayName() {
     console.log(this.name);
   }
-}("tom");
+}("tom");//相当于变量传入构造函数
 
 person.sayName();
 
-//
-
+// binding this
 class Logger {
   constructor() {
-    this.printName = this.printName.bind(this);//绑定函数，否则提取出来调用出现undefined
+    this.printName = this.printName.bind(this);
+    // 函数绑定this，提取函数出来调用时，如果函数使用this且没有进行绑定，就会出现undefined，找不到this
   }
   printName(name = "there") {
     this.print(`Hello ${name}`);
@@ -46,24 +46,20 @@ const logger = new Logger();
 const { printName } = logger;
 printName();
 
-//
-
-class MyClass {
+// set & get
+class Teacher {
   constructor() {
-    // ...
+    this.xcode;
   }
   get prop() {
-    return "getter";
+    return this.xcode;
   }
   set prop(value) {
-    console.log("setter: " + value);
+    this.xcode = value;
   }
 }
 
-let inst = new MyClass();
+let t = new Teacher();
 
-inst.prop = 123;
-// setter: 123
-
-inst.prop;
-// 'getter'
+t.prop = 'wtf';//调用set
+t.prop//调用get
