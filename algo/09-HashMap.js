96e71c24a2538035853b05da7dd14714b7f41cf7
Unknown <jamie@justek.us>
2018-07-12 17:34:44 +0800

    update

--- algo/09-HashMap.js
+++ algo/09-HashMap.js
@@ -1,41 +1,35 @@
 //散列表
 function HashTable() {
-    // put(key, value) : 向散列表增加一个新的项(也能更新散列表)
-    // remove(key) : 根据键值从散列表中移除值。
-    // get(key) : 返回根据键值检索到的特定的值。
-    var table = [];
-
-    var loseloseHashCode = function (key) {
-        var hash = 0;
-        //{1}
-        for (var i = 0; i < key.length; i++) { //{2}
-            hash += key.charCodeAt(i);
-            //{3}
-        }
-        return hash % 37; //{4}
-    };
-
-    this.put = function (key, value) {
-        var position = loseloseHashCode(key); //{5}
-        console.log(position + ' - ' + key); //{6}
-        table[position] = value; //{7}
-    };
-    this.get = function (key) {
-        return table[loseloseHashCode(key)];
-    };
-    this.remove = function (key) {
-        table[loseloseHashCode(key)] = undefined;
-    };
+  var table = [];
+  //将key转成hash
+  var loseloseHashCode = function(key) {
+    var hash = 0;
+    for (var i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % 37;
+  };
+  // 添加或更新元素
+  this.put = function(key, value) {
+    var position = loseloseHashCode(key);
+    // console.log(position)
+    table[position] = value;
+  };
+  //根据key获取元素的值
+  this.get = function(key) {
+    return table[loseloseHashCode(key)];
+  };
+  //打印指定key的值
+  this.print = function(key) {
+    console.log(this.get(key));
+  };
+  //根据key移除元素
+  this.remove = function(key) {
+    table[loseloseHashCode(key)] = undefined;
+  };
 }
 
-//test
 var hash = new HashTable();
-hash.put('Gandalf', 'gandalf@email.com');
-hash.put('John', 'johnsnow@email.com');
-hash.put('Tyrion', 'tyrion@email.com');
+hash.put("user", "tom");
 
-console.log(hash.get('Gandalf'));//gandalf@email.com
-console.log(hash.get('Loiane'));//undefined
-
-hash.remove('Gandalf');
-console.log(hash.get('Gandalf'));//undefined
+hash.print("user");
