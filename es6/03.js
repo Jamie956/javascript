b15926c58e7d7fa86d9794ef361e8c8efd84e905
Unknown <jamie@justek.us>
2018-07-13 16:14:02 +0800

    update

--- es6/03.js
+++ es6/03.js
@@ -47,3 +47,15 @@ var [a, , b] = list;
 
 //对象赋值
 var { op, lhs, rhs } = getASTNode();
+
+//  lib/math.js
+export function sum(x, y) {
+  return x + y;
+}
+export var pi = 3.141593;
+//  someApp.js
+import * as math from "lib/math";
+console.log("2π = " + math.sum(math.pi, math.pi));
+//  otherApp.js
+import { sum, pi } from "lib/math";
+console.log("2π = " + sum(pi, pi));
