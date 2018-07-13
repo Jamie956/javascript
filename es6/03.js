faa6bad5322b536b0d7d828c51ffe39d5f576838
Unknown <jamie@justek.us>
2018-07-13 14:44:03 +0800

    update

---
+++ es6/03.js
@@ -0,0 +1,12 @@
+//let 只在区块内生效
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
+//定义区块
+{
+}
+//箭头函数
+v => v + 1;
+v => ({ k: v });
+(x, y) => x + y;
+z => {}
