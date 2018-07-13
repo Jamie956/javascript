473178f4e5e77d7db57f85432a3f68b092973c85
Unknown <jamie@justek.us>
2018-07-13 15:27:28 +0800

    update

--- es6/03.js
+++ es6/03.js
@@ -2,11 +2,25 @@
 for (let index = 0; index < array.length; index++) {
   const element = array[index];
 }
+
 //定义区块
-{
-}
+{}
+
 //箭头函数
 v => v + 1;
 v => ({ k: v });
 (x, y) => x + y;
-z => {}
+z => {};
+
+//参数赋值
+function f(x, y = 7) {}
+
+//传入剩余参数
+function f(x, y, ...a) {}
+
+
+
+
+
+
+
