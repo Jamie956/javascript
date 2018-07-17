19ed8602c95028169827ceaa51b556932862a3fa
jamie <jamie@justek.us>
2018-07-17 11:46:46 +0800

    update

--- 09-regexp.js
+++ 09-regexp.js
@@ -1,18 +1,26 @@
-//替换匹配的字符
-'hello world'.replace(/h|o|d/g, '*');
-'ab123c6def2gh8ijk54lmn'.replace(/\d/g, '*');// /\d/g 多个数字
+// 类型
+// str.search(regexp);// 获取匹配字符的索引
+// str.replace(regexp, 'replace');//替换匹配的字符
+// regexp.test(str);//是否匹配
+// str.match(regexp);//获取匹配的字符
 
-//是否符合匹配
-/^\w+@[a-z0-9]+\.[a-z]+$/i.test("752492509@qq.com");
+// 特别字符
+// $ 匹配输入字符串的结尾位置
+// + 前面字符出现次数大于0
+// * 前面字符出现次数大于等于0
+// ? 前面字符出现次数0或1
 
-//获取匹配的字符
-str.match(/\d/);// 匹配第一个数字
-str.match(/\d+/);// 匹配第一个连续数字
-str.match(/\d+/g);// 匹配全部数字
-str.match(/[abc]/g);// 匹配包含a,b,c的字符
-str.match(/[a-z]/g);// 匹配a-z的字符
-str.match(/[0-9]/g);// 匹配0-9的字符
-str.match(/[^a-z]/g);// 不匹配a-z的字符
-str.match(/[^a-z0-9]/g);// 不匹配a-z和0-9的字符
+// 各种正则表达式
+var str = "abc123abc123*";
+// var a = str.match(/\d/);// 匹配第一个数字
+// var a = str.match(/\d+/);// 匹配第一个连续数字
+// var a = str.match(/\d+/g);// 匹配全部数字
+// var a = str.match(/[abc]/g);// 匹配包含a,b,c的字符
+// var a = str.match(/[a-z]/g);// 匹配a-z的字符
+// var a = str.match(/[0-9]/g);// 匹配0-9的字符
+// var a = str.match(/[^a-z]/g);// 不匹配a-z的字符
+// var a = str.match(/[^a-z0-9]/g);// 不匹配a-z和0-9的字符
+// var a = str.match(/b/); //匹配特定一个字符
+// var a = str.match(/a|2|o/g); //匹配多个指定字符
 
-"123aaaaa".search(/a/);// 获取匹配字符的索引
+console.log(a);
