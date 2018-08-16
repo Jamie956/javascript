b9e9f84f168ff7a38cc30fb84111709087c3cc3f
jamie <jamie@justek.us>
2018-08-16 17:53:40 +0800

    up

--- 03-array.js
+++ 03-array.js
@@ -1,39 +1,97 @@
+//指定位置添加数组元素
+function test01() {
+  var arr = ["a", "b", "c", "d"];
+  arr.splice(2, 0, "x");
+  console.log(arr);
+}
 
-var arr = ['a', 'b', 'c'];
+//指定位置替换数组元素
+function test02() {
+  var arr = ["a", "b", "c", "d"];
+  arr.splice(2, 1, "x");
+  console.log(arr);
+}
 
 //是否是数组
-Array.isArray(arr);
-//末尾插入元素
-arr.push('e');
-//首位插入元素
-arr.unshift('x');
-//令数组长度为0
-arr.length = 0;
-//移除数组首位元素
-arr.shift();
-//移除数组末尾元素
-arr.pop();
-//指定插入位置 (位置，长度，插入的元素)
-arr.splice(1, 2, "x", "y");
+function test03() {
+  var arr = ["a", "b", "c", "d"];
+  var rs = Array.isArray(arr);
+  console.log(rs);
+}
+
+//增删元素
+function test04() {
+  var arr = ["a", "b", "c", "d"];
+  arr.unshift("x"); //首位插入元素
+  console.log(arr);
+  arr.push("e"); //末尾插入元素
+  console.log(arr);
+  arr.shift(); //移除数组首位元素
+  console.log(arr);
+  arr.pop(); //移除数组末尾元素
+  console.log(arr);
+}
+
 //倒序
-arr.reverse();
+function test05() {
+  var arr = ["a", "b", "c", "d"];
+  arr.reverse();
+  console.log(arr);
+}
+
 //裁剪数组
-arr.slice(1, 3);
-//排序
-arr.sort();
-//以指定符号分割数组元素
-arr.join(" - ");
-//获取指定元素的索引
-arr.indexOf("b");
-//获取指定元素最后一位的索引
-arr.lastIndexOf("c");
+function test06() {
+  var arr = ["a", "b", "c", "d"];
+  var rs = arr.slice(1, 3);
+  console.log(rs);
+}
+
+//根据元素获取索引
+function test07() {
+  var arr = ["a", "b", "c", "d"];
+  var rs = arr.indexOf("b");
+  console.log(rs);
+  rs = arr.lastIndexOf("c");
+  console.log(rs);
+}
+
 //数组转字符串
-arr.toString();
+function test08() {
+  var arr = ["a", "b", "c", "d"];
+  var rs = arr.toString();
+  console.log(rs);
+}
+
 //删除数组指定位置的元素，该位置会留下空字符串
-delete arr[2];
+function test09() {
+  delete arr[2];
+}
+
 //连接数组
-arr1.concat(arr2);
+function test10() {
+  arr1.concat(arr2);
+}
+
 // 遍历数组索引
-for (index in arr) { }
+function test11() {
+  for (index in arr) {
+  }
+}
+
 // 遍历数组元素
-for (item of items) { }
+function test12() {
+  for (item of items) {
+  }
+}
+
+//排序
+function test13() {
+  arr.sort();
+}
+
+//以指定符号分割数组元素
+function test14() {
+  arr.join(" - ");
+}
+
+test08();
--- algo/04-priorityQueue.js
+++ algo/04-priorityQueue.js
@@ -1,53 +1,56 @@
 //优先队列
 function PriorityQueue() {
-    var items = [];
-    //移除首位元素
-    this.dequeue = function () {
-        return items.shift();
-    };
-    //获取首位元素
-    this.front = function () {
-        return items[0];
-    };
-    //队列是否为空
-    this.isEmpty = function () {
-        return items.length == 0;
-    };
-    //清空队列
-    this.clear = function () {
-        items = [];
-    };
-    //队列元素个数
-    this.size = function () {
-        return items.length;
-    };
-    this.print = function () {
-        console.log(items);
-    };
-    //内部函数
-    function QueueElement(element, priority) {
-        this.element = element;
-        this.priority = priority;
-    }
-    //根据优先度加入元素
-    this.enqueue = function (element, priority) {
-        var queueElement = new QueueElement(element, priority);
-        if (this.isEmpty()) {//数组为空时
-            items.push(queueElement);//末尾加入元素
-        } else {
-            var added = false;
-            for (var i = 0; i < items.length; i++) {
-                if (queueElement.priority < items[i].priority) {
-                    items.splice(i, 0, queueElement);//插入到指定位置
-                    added = true;
-                    break;
-                }
-            }
-            if (!added) {//插入元素优先度排最后
-                items.push(queueElement);//末尾加入元素
-            }
+  var items = [];
+  //根据优先度加入元素
+  this.enqueue = function(element, priority) {
+    var queueElement = new QueueElement(element, priority);
+    if (this.isEmpty()) {
+      items.push(queueElement); //末尾加入元素
+    } else {
+      var added = false;
+      for (var i = 0; i < items.length; i++) {
+        if (queueElement.priority < items[i].priority) {
+          items.splice(i, 0, queueElement); //插入到指定位置
+          added = true;
+          break;
         }
-    };
+      }
+      if (!added) {
+        //插入元素优先度排最后
+        items.push(queueElement); //末尾加入元素
+      }
+    }
+  };
+
+  //移除首位元素
+  this.dequeue = function() {
+    return items.shift();
+  };
+  //清空队列
+  this.clear = function() {
+    items = [];
+  };
+
+  //获取首位元素
+  this.front = function() {
+    return items[0];
+  };
+  //队列是否为空
+  this.isEmpty = function() {
+    return items.length == 0;
+  };
+  //队列元素个数
+  this.size = function() {
+    return items.length;
+  };
+  this.print = function() {
+    console.log(items);
+  };
+  //内部函数
+  function QueueElement(element, priority) {
+    this.element = element;
+    this.priority = priority;
+  }
 }
 
 var priorityQueue = new PriorityQueue();
