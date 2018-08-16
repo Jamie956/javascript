378afd0a7861ba90d5290e8d87714f7fc2f6cff7
jamie <jamie@justek.us>
2018-08-16 11:57:09 +0800

    up

--- es6/01-base.js
+++ es6/01-base.js
@@ -11,13 +11,13 @@ for (let index = 0; index < array.length; index++) {
 v => v + 1;
 v => ({ k: v });
 (x, y) => x + y;
-z => { };
+z => {};
 
 //参数赋值
-function f(x, y = 7) { }
+function f(x, y = 7) {}
 
 //传入剩余参数
-function f(x, y, ...a) { }
+function f(x, y, ...a) {}
 
 //...传参
 var params = ["hello", true, 7];
@@ -36,8 +36,8 @@ obj = { x, y };
 
 //对象内定义函数格式
 obj = {
-  foo(a, b) { },
-  bar(x, y) { }
+  foo(a, b) {},
+  bar(x, y) {}
 };
 
 //数组赋值
@@ -49,53 +49,46 @@ var [a, , b] = list;
 var { op, lhs, rhs } = getASTNode();
 
 //lib/math.js
-//导出函数
-export function sum(x, y) {
+function sum(x, y) {
   return x + y;
 }
-//导出变量
-export var pi = 3.141593;
-//导入js，全部导入
-import * as math from "lib/math";
-math.sum(math.pi, math.pi)
-//导入js，导入指定函数或变量
-import { sum, pi } from "lib/math";
-sum(pi, pi)
-
-//导出一个js
-export * from "lib/math"
+var pi = 3.141593;
+//导出变量和函数
+export { sum, pi };
 //默认导出
-export default (x) => Math.exp(x)
-//导入默认导出函数，导入指定函数或变量
-import exp, { pi, e } from "lib/mathplusplus"
+export default () => {};
+
+//导入默认导出函数,指定函数,变量
+import math, { sum, pi } from "lib/math";
 
 //set集合
-let s = new Set()
-s.add("hello").add("goodbye")
-s.size
-s.has("hello")
-s.values()
-for (let key of s.values()) { }
+let s = new Set();
+s.add("hello").add("goodbye");
+s.size;
+s.has("hello");
+s.values();
+for (let key of s.values()) {
+}
 
 //对象结合
-var dest = { quux: 0 }
-var src1 = { foo: 1, bar: 2 }
-var src2 = { foo: 3, baz: 4 }
-Object.assign(dest, src1, src2)
-dest.quux === 0
-dest.foo === 3
-dest.bar === 2
-dest.baz === 4
+var dest = { quux: 0 };
+var src1 = { foo: 1, bar: 2 };
+var src2 = { foo: 3, baz: 4 };
+Object.assign(dest, src1, src2);
+dest.quux === 0;
+dest.foo === 3;
+dest.bar === 2;
+dest.baz === 4;
 
-arr.find(x => x > 3)// 数组元素遍历
-arr.findIndex(x => x > 3)// 数组索引遍历
+arr.find(x => x > 3); // 数组元素遍历
+arr.findIndex(x => x > 3); // 数组索引遍历
 
 //proxy
-let target = { foo: "Welcome, foo" }
+let target = { foo: "Welcome, foo" };
 let proxy = new Proxy(target, {
   get(receiver, name) {
-    return name in receiver ? receiver[name] : `Hello, ${name}`
+    return name in receiver ? receiver[name] : `Hello, ${name}`;
   }
-})
-proxy.foo === "Welcome, foo"
-proxy.world === "Hello, world"
+});
+proxy.foo === "Welcome, foo";
+proxy.world === "Hello, world";
