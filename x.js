68dafc77d1eb441bde845a71cedc420ea563f524
Jamie956 <752492509@qq.com>
2018-08-20 11:23:26 +0800

    up

---
+++ x.js
@@ -0,0 +1,10 @@
+		//限制用户连续点击
+    function throttle(fun) {
+			if (fun.timeoutId) {
+				clearTimeout(fun.timeoutId);
+			}
+			fun.timeoutId = setTimeout(function () {
+				fun();
+				fun.timeoutId = null;
+			}, 1000);
+		}
