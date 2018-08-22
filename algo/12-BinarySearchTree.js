a5813ed649bbd44f58f42ceb34c0c2ac3b196cc8
jamie <jamie@justek.us>
2018-08-22 11:12:52 +0800

    up

--- algo/12-BinarySearchTree.js
+++ algo/12-BinarySearchTree.js
@@ -1,67 +1,84 @@
 //二叉树
 function BinarySearchTree() {
-  var Node = function (key) {
+  //节点
+  var Node = function(key) {
     this.key = key;
     this.left = null;
     this.right = null;
   };
   var root = null;
+
   // 向树中插入一个新的键
-  this.insert = function (key) {
-    var newNode = new Node(key); //{1}
-    if (root === null) { //{2}
+  this.insert = function(key) {
+    var newNode = new Node(key);
+    //情况一,创建根节点
+    if (root === null) {
       root = newNode;
     } else {
-      insertNode(root, newNode); //{3}
+      //情况二,存在根节点
+      insertNode(root, newNode);
     }
   };
-  var insertNode = function (node, newNode) {
-    if (newNode.key < node.key) { //{4}
+
+  var insertNode = function(node, newNode) {
+    if (newNode.key < node.key) {
+      //情况一,新节点的key小于指定节点,指定节点的左节点为空
       if (node.left === null) {
         node.left = newNode;
       } else {
-        insertNode(node.left, newNode); //{7}
+        //情况二,新节点的key小于指定节点,指定节点的左节点不为空,继续递归寻找下一个节点
+        insertNode(node.left, newNode);
       }
     } else {
-      if (node.right === null) { //{8}
-        node.right = newNode; //{9}
+      if (node.right === null) {
+        //情况三,新节点的key大于指定节点,指定节点的右节点为空
+        node.right = newNode;
       } else {
-        insertNode(node.right, newNode); //{10}
+        //情况四,新节点的key大于指定节点,指定节点的右节点不为空,继续递归寻找下一个节点
+        insertNode(node.right, newNode);
       }
     }
   };
-  // search(key) :在树中查找一个键,如果节点存在,则返回 true ;如果不存在,则返回 false
-  this.search = function (key) {
-    return searchNode(root, key); //{1}
+
+  //从根节点开始搜索,判断是否存在指定的key
+  this.search = function(key) {
+    return searchNode(root, key);
   };
-  var searchNode = function (node, key) {
-    if (node === null) { //{2}
+
+  var searchNode = function(node, key) {
+    //情况一,当前节点为空,key不存在
+    if (node === null) {
       return false;
     }
-    if (key < node.key) { //{3}
-      return searchNode(node.left, key); //{4}
-    } else if (key > node.key) { //{5}
-      return searchNode(node.right, key); //{6}
+    //情况二,指定的key小于当前节点的key,用左节点继续递归寻找
+    if (key < node.key) {
+      return searchNode(node.left, key);
+    } else if (key > node.key) {
+      //情况三,指定的key大于当前节点的key,用右节点继续递归寻找
+      return searchNode(node.right, key);
     } else {
-      return true; //{7}
+      //情况四,指定的key等于当前节点的key,指定的key存在
+      return true;
     }
   };
+
   // 中序遍历
-  this.inOrderTraverse = function (callback) {
+  this.inOrderTraverse = function(callback) {
     inOrderTraverseNode(root, callback);
   };
-  var inOrderTraverseNode = function (node, callback) {
+  var inOrderTraverseNode = function(node, callback) {
     if (node !== null) {
       inOrderTraverseNode(node.left, callback);
       callback(node.key);
       inOrderTraverseNode(node.right, callback);
     }
   };
+
   // 先序遍历
-  this.preOrderTraverse = function (callback) {
+  this.preOrderTraverse = function(callback) {
     preOrderTraverseNode(root, callback);
   };
-  var preOrderTraverseNode = function (node, callback) {
+  var preOrderTraverseNode = function(node, callback) {
     if (node !== null) {
       callback(node.key);
       preOrderTraverseNode(node.left, callback);
@@ -70,21 +87,22 @@ function BinarySearchTree() {
   };
 
   // 后序遍历
-  this.postOrderTraverse = function (callback) {
+  this.postOrderTraverse = function(callback) {
     postOrderTraverseNode(root, callback);
   };
-  var postOrderTraverseNode = function (node, callback) {
+  var postOrderTraverseNode = function(node, callback) {
     if (node !== null) {
       postOrderTraverseNode(node.left, callback);
       postOrderTraverseNode(node.right, callback);
       callback(node.key);
     }
   };
+
   // 最小值
-  this.min = function () {
+  this.min = function() {
     return minNode(root);
   };
-  var minNode = function (node) {
+  var minNode = function(node) {
     if (node) {
       while (node && node.left !== null) {
         node = node.left;
@@ -93,11 +111,12 @@ function BinarySearchTree() {
     }
     return null;
   };
+
   // 最大值
-  this.max = function () {
+  this.max = function() {
     return maxNode(root);
   };
-  var maxNode = function (node) {
+  var maxNode = function(node) {
     if (node) {
       while (node && node.right !== null) {
         node = node.right;
@@ -106,31 +125,39 @@ function BinarySearchTree() {
     }
     return null;
   };
+
   // remove(key) :从树中移除某个键。
-  this.remove = function (key) {
+  this.remove = function(key) {
     root = removeNode(root, key); //{1}
   };
-  var removeNode = function (node, key) {
-    if (node === null) { //{2}
+  var removeNode = function(node, key) {
+    if (node === null) {
+      //{2}
       return null;
     }
-    if (key < node.key) { //{3}
+    if (key < node.key) {
+      //{3}
       node.left = removeNode(node.left, key); //{4}
       return node; //{5}
-    } else if (key > node.key) { //{6}
+    } else if (key > node.key) {
+      //{6}
       node.right = removeNode(node.right, key); //{7}
       return node; //{8}
-    } else { //键等于node.key
+    } else {
+      //键等于node.key
       //第一种情况——一个叶节点
-      if (node.left === null && node.right === null) { //{9}
+      if (node.left === null && node.right === null) {
+        //{9}
         node = null; //{10}
         return node; //{11}
       }
       //第二种情况——一个只有一个子节点的节点
-      if (node.left === null) { //{12}
+      if (node.left === null) {
+        //{12}
         node = node.right; //{13}
         return node; //{14}
-      } else if (node.right === null) { //{15}
+      } else if (node.right === null) {
+        //{15}
         node = node.left; //{16}
         return node; //{17}
       }
@@ -159,7 +186,8 @@ tree.insert(20);
 tree.insert(18);
 tree.insert(25);
 
-function printNode(value) { //{6}
+function printNode(value) {
+  //{6}
   console.log(value);
 }
 tree.inOrderTraverse(printNode); //{7}
