8c1e85a431de283ddd1126bb6604c0da7e1ed0e3
jamie <752492509@qq.com>
2018-10-29 10:24:13 +0800

    up

--- call/index.js
+++ call/index.js
@@ -1,4 +1,20 @@
-function say(content) {
-  console.log("From " + this + ": Hello " + content);
+function test01() {
+  function say(content) {
+    console.log("From " + this + ": Hello " + content);
+  }
+  say.call("Bob", "World"); //From Bob: Hello World
 }
-say.call("Bob", "World"); //From Bob: Hello World
+
+function test02() {
+  var person = {
+    name: "caibirdme",
+    run: function(time) {
+      console.log(this.name + "has been running for over " + time + " minutes");
+    }
+  };
+  person.run(30);
+  // 等价于
+  person.run.call(person, 30);
+}
+
+test02();
--- this/index.js
+++ this/index.js
@@ -20,16 +20,4 @@ function test02() {
   console.log(obj.x); // 1
 }
 
-function test03() {
-  var person = {
-    name: "caibirdme",
-    run: function(time) {
-      console.log(this.name + "has been running for over " + time + " minutes");
-    }
-  };
-  person.run(30);
-  // 等价于
-  person.run.call(person, 30);
-}
-
-test03();
+test02();
