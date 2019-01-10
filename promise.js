c9eb79a593003ab60c8db516718dcc724729fccc
Jamie <752492509@qq.com>
2019-01-10 16:04:41 +0800

    up

--- promise.js
+++ promise.js
@@ -1,100 +1,31 @@
-//创建pomise对象，返回resolve
 function test01() {
-  var promise = new Promise(resolve => {
+  new Promise((resolve, reject) => {
     resolve(42);
-  });
-  promise.then(res => {
-    console.log(res);
-  });
+    // reject(new Error("Something Wrong"));
+  })
+    .then(res => {
+      console.log(res);
+    })
+    .catch(e => {
+      console.log(e);
+    });
 }
 
-//创建pomise对象，返回resolve
 function test02() {
-  var promise = new Promise(reject => {
-    reject(new Error("Something Wrong"));
-  });
-  promise
-    .then(res => {
-      console.log(res);
-    })
-    .catch(e => {
-      console.log(e);
-    });
-}
-
-//创建pomise对象，返回resolve或reject
-function test03() {
-  var promise = new Promise((resolve, reject) => {
-    if (true) {
-      resolve(42);
-    } else {
-      reject(new Error("Something Wrong"));
-    }
-  });
-  promise
-    .then(res => {
-      console.log(res);
-    })
-    .catch(e => {
-      console.log(e);
-    });
-}
-
-//调用异步函数，返回promise对象
-function test04() {
-  function asyncFun() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve("Got it");
-      }, 2000);
-    });
-  }
-  asyncFun()
-    .then(data => {
-      console.log(data);
-    })
-    .catch(e => {
-      console.log(e);
-    });
-  console.log("Go on");
-}
-
-//promise语法糖，返回resolve
-function test05() {
-  var promise = Promise.resolve(42);
-  promise.then(res => {
+  Promise.resolve(42).then(res => {
     console.log(res);
   });
 }
 
-//promise语法糖，返回reject
-function test06() {
-  var promise = Promise.reject(new Error("Something Wrong"));
-  promise.catch(e => {
+function test03() {
+  Promise.reject(new Error("Something Wrong")).catch(e => {
     console.log(e);
   });
 }
 
-//调用链错误处理
-function test07() {
-  var promise = Promise.resolve();
-
-  promise
-    .then(() => console.log("task a"))
-    .then(() => {
-      console.log("task b");
-      // throw new Error("task b meet with error");
-    })
-    .then(() => console.log("finish"))
-    .catch(e => {
-      console.log(e);
-    });
-}
-
 //调用链传递返回值
-function test08() {
-  var promise = Promise.resolve(1);
-  promise
+function test04() {
+  Promise.resolve(1)
     .then(val => {
       return val * 2;
     })
@@ -103,14 +34,11 @@ function test08() {
     })
     .then(val => {
       console.log(val);
-    })
-    .catch(e => {
-      console.log(e);
     });
 }
 
 //promise all,多个resolve 的值返回一个数组
-function test09() {
+function test05() {
   function getData(URL) {
     return new Promise((resolve, reject) => {
       if (true) {
@@ -126,10 +54,10 @@ function test09() {
 }
 
 //promise all
-function test10() {
+function test06() {
   function timerPromisefy(delay) {
-    return new Promise(function(resolve) {
-      setTimeout(function() {
+    return new Promise(resolve => {
+      setTimeout(() => {
         resolve(delay);
       }, delay);
     });
@@ -148,7 +76,7 @@ function test10() {
 }
 
 //promise race
-function test11() {
+function test7() {
   function timerPromisefy(delay) {
     return new Promise(function(resolve) {
       setTimeout(function() {
@@ -165,4 +93,4 @@ function test11() {
   ]).then(value => console.log(value));
 }
 
-test11();
+test06();
