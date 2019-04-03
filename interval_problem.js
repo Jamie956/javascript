0bb8adf1c86061f2c81e5afa2f66c321fb6c5532
Jamie <752492509@qq.com>
2019-04-03 17:54:42 +0800

    up

---
+++ interval_problem.js
@@ -0,0 +1,11 @@
+// setInterval(() => {
+//   setTimeout(() => {
+//     console.log("Execute after 10s " + new Date);
+//   }, 10000);
+// }, 200);
+
+setInterval(() => {
+  setTimeout(() => {
+    console.log("Execute after 10s " + new Date);
+  }, 1000);
+}, 1000);
