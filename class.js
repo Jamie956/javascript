9ec7af093c1c41e839e69ccd1af3ea6316eb7751
Jamie <752492509@qq.com>
2019-05-24 17:47:46 +0800

    up

--- class.js
+++ class.js
@@ -1,87 +1,59 @@
 function test01() {
-  class Point {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
+  class Greeting {
+    constructor(word) {
+      this.word = word;
     }
-    print() {
-      console.log("(" + this.x + ", " + this.y + ")");
+    say() {
+      console.log(this.word);
     }
   }
-  var p = new Point(2, 3);
-  p.print();
+  var greet = new Greeting("hi");
+  greet.say();
 }
 
 function test02() {
-  class Person {
+  class Greeting {
     constructor() {
-      //bind使绑定的函数在改变作用域操作时仍然指向Person的this
-      this.print = this.print.bind(this);
+      this.x = 1;
+      // say.this -> Person.this
+      this.say = this.say.bind(this);
     }
-    print() {
-      console.log(this);
+    say() {
+      console.log(this.x);
     }
   }
-  const p = new Person();
-  var print = p.print;//this -> window (if no bind)
-  print();
+  const greet = new Greeting();
+  var say = greet.say;
+  say();
 }
 
-//===set & get===
 function test03() {
-  class Person {
-    constructor() {
-      this.name;
-      this.email;
-    }
-    get prop() {
-      return this;
-    }
-    set prop(person) {
-      this.name = person.name;
-      this.email = person.email;
-    }
-  }
-
-  let p = new Person();
-  //set
-  p.prop = {
-    name: "tom",
-    email: "tomeamil"
-  };
-  //get
-  console.log(p.prop);
-}
-
-function test04() {
   class Father {
     constructor(x) {
       this.x = x;
     }
-    fatherPrint() {
+    fatherSay() {
       console.log(this.x);
     }
   }
 
   class Son extends Father {
     constructor(x, y) {
-      super(x); //调用父构造函数
+      super(x);
       this.y = y;
     }
-    //调用父函数
-    invokeFatherPrint() {
-      return this.fatherPrint();
+    invokeFather() {
+      return this.fatherSay();
     }
-    //调用父属性x
-    sonPrint() {
-      console.log(this.y + this.x);
+    sonSay() {
+      console.log(this.x + this.y);
     }
   }
 
-  const s = new Son("x1", "y1");
-  s.invokeFatherPrint();
-  s.sonPrint();
-  s.fatherPrint(); //直接调用父类方法
+  const s = new Son("1", "2");
+  s.sonSay();
+  s.invokeFather();
+  s.fatherSay();
 }
 
-test04();
+test03();
