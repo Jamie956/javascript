3d0a492418fd68e2756ad77c8584a03275eb8e4e
Jamie <752492509@qq.com>
2019-05-24 17:15:17 +0800

    up

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,80 +1,72 @@
-function resolveAfter2Seconds() {
+function timer(content, time) {
+  console.log("start " + content);
   return new Promise(resolve => {
     setTimeout(() => {
-      resolve('resolved');
-    }, 2000);
+      console.log(content + " resolve");
+      resolve(content);
+    }, time);
   });
 }
 
-async function asyncCall() {
-  console.log('calling');
-  var result = await resolveAfter2Seconds();
-  console.log(result);
+function t1() {
+  console.log("start");
+  var a = timer("a", 2000);
+  console.log(a);
+  /*
+  start
+  start a
+  Promise { <pending> }
+  a resolve
+  */
 }
 
-asyncCall();
-
-
-
-//////////////////////////////
-
-var resolveAfter2Seconds = function() {
-  console.log("starting slow promise");
-  return new Promise(resolve => {
-    setTimeout(function() {
-      resolve(20);
-      console.log("slow promise is done");
-    }, 2000);
-  });
-};
-
-var resolveAfter1Second = function() {
-  console.log("starting fast promise");
-  return new Promise(resolve => {
-    setTimeout(function() {
-      resolve(10);
-      console.log("fast promise is done");
-    }, 1000);
-  });
-};
-
-var sequentialStart = async function() {
-  console.log('==SEQUENTIAL START==');
-  // If the value of the expression following the await operator is not a Promise, it's converted to a resolved Promise.
-  const slow = await resolveAfter2Seconds();
-  const fast = await resolveAfter1Second();
-
-  console.log(slow);
-  console.log(fast);
+// only async
+async function t2() {
+  console.log("start");
+  var a = timer("a", 2000);
+  console.log(a);
+  /*
+  start
+  start a
+  Promise { <pending> }
+  a resolve
+  */
 }
 
-var concurrentStart = async function() {
-  console.log('==CONCURRENT START with await==');
-  const slow = resolveAfter2Seconds(); // starts timer immediately
-  const fast = resolveAfter1Second();
-
-  console.log(await slow);
-  console.log(await fast); // waits for slow to finish, even though fast is already done!
+// async & await function
+async function t3() {
+  console.log("start");
+  var a = await timer("a", 1000);
+  var b = await timer("b", 2000);
+  console.log(a);
+  console.log(b);
+  /*
+  start
+  start a
+  a resolve
+  start b
+  b resolve
+  a
+  b
+  */
 }
 
-var stillConcurrent = function() {
-  console.log('==CONCURRENT START with Promise.all==');
-  Promise.all([resolveAfter2Seconds(), resolveAfter1Second()]).then((messages) => {
-    console.log(messages[0]); // slow
-    console.log(messages[1]); // fast
-  });
+// async & await value
+async function t4() {
+  console.log("start");
+  var a = timer("a", 2000);
+  var b = timer("b", 1000);
+  console.log(await a);
+  console.log(await b);
+  /*
+  start
+  start a
+  start b
+  b resolve
+  a resolve
+  a
+  b
+  */
 }
 
-var parallel = function() {
-  console.log('==PARALLEL with Promise.then==');
-  resolveAfter2Seconds().then((message)=>console.log(message));
-  resolveAfter1Second().then((message)=>console.log(message));
-}
-
-sequentialStart(); // after 2 seconds, logs "slow", then after 1 more second, "fast"
-// wait above to finish
-setTimeout(concurrentStart, 4000); // after 2 seconds, logs "slow" and then "fast"
-// wait again
-setTimeout(stillConcurrent, 7000); // same as concurrentStart
-// wait again
-setTimeout(parallel, 10000); // trully parallel: after 1 second, logs "fast", then after 1 more second, "slow"
+t4();
