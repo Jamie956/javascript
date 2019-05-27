31fbc45a5a0565c5fe915438f14be7f02fd46ccd
Jamie <752492509@qq.com>
2019-05-27 16:45:11 +0800

    up

--- closure.js
+++ closure.js
@@ -1,4 +1,5 @@
 // 读取函数内部变量
+// 返回函数
 function test01() {
   function f1() {
     var j = 4;
@@ -10,25 +11,7 @@ function test01() {
   result();
 }
 
-// function test02() {
-//   function makeAdder(x) {
-//     return function(y) {
-//       return x + y;
-//     };
-//   }
-
-//   var add5 = makeAdder(5);
-//   var add10 = makeAdder(10);
-
-//   console.log(add5(2)); // 7
-//   console.log(add10(2)); // 12
-
-//   // 释放对闭包的引用
-//   add5 = null;
-//   add10 = null;
-// }
-
-function test03() {
+function test02() {
   //立即执行的匿名函数
   var Counter = (function() {
     var privateCounter = 0;
@@ -39,9 +22,6 @@ function test03() {
       increment: function() {
         changeBy(1);
       },
-      decrement: function() {
-        changeBy(-1);
-      },
       value: function() {
         return privateCounter;
       }
@@ -50,27 +30,7 @@ function test03() {
 
   console.log(Counter.value()); //0
   Counter.increment();
-  Counter.increment();
-  console.log(Counter.value()); //2
-  Counter.decrement();
   console.log(Counter.value()); //1
-}
-
-function test04() {
-  var singleton = (function() {
-    var age = 22;
-    var speak = function() {
-      console.log("speaking!!!");
-    };
-
-    return {
-      name: "percy",
-      getAge: function() {
-        return age;
-      }
-    };
-  })();
-  console.log(singleton.name);
 }
 
 test01();
--- promise.js
+++ promise.js
@@ -1,30 +1,37 @@
-function test01() {
+// resolve
+function t1() {
   new Promise((resolve, reject) => {
     resolve(42);
-    // reject(new Error("Something Wrong"));
-  })
-    .then(res => {
-      console.log(res);
-    })
-    .catch(e => {
-      console.log(e);
-    });
+  }).then(res => {
+    console.log(res);
+  });
 }
 
-function test02() {
+// reject
+function t2() {
+  new Promise((resolve, reject) => {
+    reject(new Error("Something Wrong"));
+  }).catch(e => {
+    console.log(e);
+  });
+}
+
+// no new resolve
+function t3() {
   Promise.resolve(42).then(res => {
     console.log(res);
   });
 }
 
-function test03() {
+// no new reject
+function t4() {
   Promise.reject(new Error("Something Wrong")).catch(e => {
     console.log(e);
   });
 }
 
 //调用链传递返回值
-function test04() {
+function t5() {
   Promise.resolve(1)
     .then(val => {
       return val * 2;
@@ -37,60 +44,25 @@ function test04() {
     });
 }
 
-//promise all,多个resolve 的值返回一个数组
-function test05() {
-  function getData(URL) {
-    return new Promise((resolve, reject) => {
-      if (true) {
-        resolve(URL);
-      } else {
-        reject(new Error("Something Wrong"));
-      }
-    });
+//promise all
+function t6() {
+  function Add(URL) {
+    return Promise.resolve(URL);
   }
-  Promise.all([getData("baidu.com"), getData("google.com")])
-    .then(val => console.log(val))
-    .catch(e => console.log(e));
+  Promise.all([Add("a"), Add("b")]).then(val => console.log(val));
 }
 
-//promise all
-function test06() {
-  function timerPromisefy(delay) {
+//promise race
+function t7() {
+  function task(delay) {
     return new Promise(resolve => {
       setTimeout(() => {
         resolve(delay);
       }, delay);
     });
   }
-  var startDate = Date.now();
-  // 异步,所有promise变为resolve后程序退出
-  Promise.all([
-    timerPromisefy(1),
-    timerPromisefy(32),
-    timerPromisefy(64),
-    timerPromisefy(128)
-  ]).then(res => {
-    console.log(Date.now() - startDate + "ms");
-    console.log(res);
-  });
-}
-
-//promise race
-function test7() {
-  function timerPromisefy(delay) {
-    return new Promise(function(resolve) {
-      setTimeout(function() {
-        resolve(delay);
-      }, delay);
-    });
-  }
   // 任何一个promise变为resolve或reject 的话程序就停止运行
-  Promise.race([
-    timerPromisefy(1),
-    timerPromisefy(32),
-    timerPromisefy(64),
-    timerPromisefy(128)
-  ]).then(value => console.log(value));
+  Promise.race([task(1), task(32), task(64)]).then(rs => console.log(rs));
 }
 
-test06();
+t7();
