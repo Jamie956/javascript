a2a9fe7a3962db74b88af86d1d9d12ee5ac288c7
jamie956 <752492509@qq.com>
2019-11-22 22:30:50 +0800

    up

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 let p = new Proxy({ a: 1 }, {
-    get(target, name) {
-        return name in target ? target[name] : 6;
-    }
+	get(target, name) {
+		return name in target ? target[name] : 6;
+	}
 });
 console.log(p.a);
 console.log(p.b);
@@ -10,20 +10,20 @@ console.log(p.c);
 
 //
 let validator = {
-    set: function (obj, prop, value) {
-        if (prop === 'age') {
-            if (!Number.isInteger(value)) {
-                throw new TypeError('The age is not an integer');
-            }
-            if (value > 200) {
-                throw new RangeError('The age seems invalid');
-            }
-        }
-        // The default behavior to store the value
-        obj[prop] = value;
-        // 表示成功
-        return true;
-    }
+	set: function (obj, prop, value) {
+		if (prop === 'age') {
+			if (!Number.isInteger(value)) {
+				throw new TypeError('The age is not an integer');
+			}
+			if (value > 200) {
+				throw new RangeError('The age seems invalid');
+			}
+		}
+		// The default behavior to store the value
+		obj[prop] = value;
+		// 表示成功
+		return true;
+	}
 };
 let person = new Proxy({}, validator);
 person.age = 100;
