42721a9864fea1f1ef4182178556cb821ab19bf8
jamie956 <752492509@qq.com>
2019-12-08 00:02:52 +0800

    [ds] loop queue pro

---
+++ data_struct/loop_queue_pro.js
@@ -0,0 +1,82 @@
+/**
+ * Initialize your data structure here. Set the size of the queue to be k.
+ * @param {number} k
+ */
+var MyCircularQueue = function (k) {
+  this.queue = new Array(k);
+  this.head = -1;
+  this.tail = -1;
+  this.size = k;
+  return this;
+};
+
+/**
+* Insert an element into the circular queue. Return true if the operation is successful. 
+* @param {number} value
+* @return {boolean}
+*/
+MyCircularQueue.prototype.enQueue = function (value) {
+  if (this.isFull()) {
+    return false;
+  }
+  if(this.isEmpty()){
+    this.head = 0;
+  }
+  this.tail = (this.tail + 1) % this.size;
+  this.queue[this.tail] = value;
+  return true;
+};
+
+/**
+* Delete an element from the circular queue. Return true if the operation is successful.
+* @return {boolean}
+*/
+MyCircularQueue.prototype.deQueue = function () {
+  if (this.isEmpty()) {
+    return false;
+  }
+  if (this.head == this.tail) {
+    this.head = -1;
+    this.tail = -1;
+    return true;
+  }
+  this.head = (this.head + 1) % this.size;
+  return true;
+};
+
+/**
+* Get the front item from the queue.
+* @return {number}
+*/
+MyCircularQueue.prototype.Front = function () {
+  return this.isEmpty() ? -1 : this.queue[this.head];
+};
+
+/**
+* Get the last item from the queue.
+* @return {number}
+*/
+MyCircularQueue.prototype.Rear = function () {
+  return this.isEmpty() ? -1 : this.queue[this.tail];
+};
+
+/**
+* Checks whether the circular queue is empty or not.
+* @return {boolean}
+*/
+MyCircularQueue.prototype.isEmpty = function () {
+  return this.head == -1;
+};
+
+/**
+* Checks whether the circular queue is full or not.
+* @return {boolean}
+*/
+MyCircularQueue.prototype.isFull = function () {
+  return ((this.tail + 1) % this.size) == this.head;
+};
+
+let myCircularQueue = new MyCircularQueue(3);
+myCircularQueue.enQueue(1);
+myCircularQueue.enQueue(2);
+myCircularQueue.enQueue(3);
